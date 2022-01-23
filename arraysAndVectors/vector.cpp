f4ad7d0b0ead6eb5ef89f42110461f6207cb463c
sumana2001 <sumana.basu2001@gmail.com>
2022-01-23 11:58:35 +0530

    Feat: Vector of Vector

--- arraysAndVectors/vector.cpp
+++ arraysAndVectors/vector.cpp
@@ -11,7 +11,7 @@ int main()
     // vector<int> arr = {1, 2, 3, 4, 5, 6};
 
     //fill constructor
-    vector<int> arr(10,0);
+    vector<int> arr(5,0);
 
     //pop_back O(1)
     arr.pop_back();
@@ -29,6 +29,25 @@ int main()
 
     //capacity of vector
     cout<<arr.capacity()<<endl;
+
+    //2D array
+    vector<vector<int>> arr2={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9,10},
+        {11,12}
+    };
+
+    //update value
+    arr2[0][0]+=10;
+
+    //print all values
+    for(int i=0;i<arr2.size();i++){
+        for(int num:arr2[i]){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
