ac7e97061059d32a05fc3006ec09ed9927f6529c
sumana2001 <sumana.basu2001@gmail.com>
2022-07-21 17:38:37 +0530

    code

--- dp/palindromicPartition.cpp
+++ dp/palindromicPartition.cpp
@@ -31,10 +31,26 @@ int minPalindromicPartition(string s, int i, int j){
     }
     return min;
 }
+vector<vector<int>> dp(101,vector<int>(101,-1));
+int minPalindromicPartitionRecursive(string s,int i, int j){
+    if(dp[i][j]!=-1)
+        return dp[i][j];
+    if(i>=j || isPalindrome(s,i,j))
+        return 0;
+    int min=INT_MAX;
+    int count;
+    for(int k=i;k<j;k++){
+        count=minPalindromicPartitionRecursive(s,i,k)+minPalindromicPartitionRecursive(s,k+1,j)+1;
+        if(count<min)
+            min=count;
+    }
+    return dp[i][j]=min;
+}
 
 int main(){
-    string s="abcde";
+    string s="aaaa";
     int n=s.length();
     cout<<minPalindromicPartition(s,0,n-1);
+    cout<<minPalindromicPartitionRecursive(s,0,n-1);
     return 0;
 }
